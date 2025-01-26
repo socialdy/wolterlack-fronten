037f6756412fc27f304ec96ed2f2c06487ac4bc4
socialdy <markuswallner19@hotmail.com>
2025-01-26 19:50:26 +0100

    added ct form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,7 +1,23 @@
-import Cal from "@calcom/embed-react";
+/* If you are using npm */
+// npm install @calcom/embed-react
 
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
 
 export const ContactForm = () => {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({ namespace: "15min" });
+      cal("ui", {
+        cssVarsPerTheme: {
+          light: { "cal-brand": "#1E1E1E" },
+          dark: { "cal-brand": "#fafafa" },
+        },
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    })();
+  }, []);
 
   return (
     <section className="py-20 bg-gray-50" id="beratung">
@@ -11,7 +27,12 @@ export const ContactForm = () => {
           <p className="text-gray-600 mb-7">
             Lassen Sie uns Ihre Traumküche planen! Füllen Sie das Formular aus und buchen Sie einen Termin, der Ihnen passt.
           </p>
-          <Cal calLink="rick/get-rick-rolled" config={{ theme: "light" }}></Cal>
+          <Cal
+            namespace="15min"
+            calLink="kuechenstudiobergheim/15min"
+            style={{ width: "100%", height: "100%", overflow: "scroll" }}
+            config={{ layout: "month_view" }}
+          ></Cal>
         </div>
       </div>
     </section>
