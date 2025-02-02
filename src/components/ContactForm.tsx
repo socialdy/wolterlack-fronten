f35a87c8b60a635bae71f8ac18c170ce7a7f2787
socialdy <markuswallner19@hotmail.com>
2025-02-02 22:37:05 +0100

    changed ct 2

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -23,7 +23,7 @@ export const ContactForm = () => {
     <section className="py-20 bg-gray-50" id="beratung">
       <div className="container max-w-4xl mx-auto px-4">
         <div className="text-center mb-12">
-          <h2 className="text-4xl font-semibold text-gray-800 mb-4">Buchen Sie jetzt Ihren Termin!</h2>
+          <h2 className="text-4xl font-light text-center uppercase mb-4">Buchen Sie jetzt Ihren Termin!</h2>
           <p className="text-gray-600 mb-7">
             Lassen Sie uns Ihre Traumküche planen! Füllen Sie das Formular aus und buchen Sie einen Termin, der Ihnen passt.
           </p>
